1092912a07bd83885c77110f43e892d835326659
Laercio Paschoal Petreca <laerciopaschoal@hotmail.com>
2026-03-08 15:15:46 -0300

    index

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const startYear = 2008;
 const currentYear = new Date().getFullYear();
 const experience = currentYear - startYear;
 document.getElementById("experienceYears").innerText =
-  "Mais de " + experience + " anos de experiência em infraestrutura e tecnologia.";
+  "Profissional com mais de " + experience + " anos de experiência em infraestrutura, virtualização, storage e cloud computing. Especialista em Kubernetes, AWS, Azure, DevOps e automação de infraestrutura."
